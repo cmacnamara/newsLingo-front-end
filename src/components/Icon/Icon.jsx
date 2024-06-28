03a5b3fb1b95ef6c211dcb29bedb798a85af8c97
Eunice Y <euniceyang21@gmail.com>
2024-06-28 10:24:26 -0500

    Update styling of word cards (#60)
    * Update styling of word cards
    
    * Reshape cards

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,16 +1,16 @@
 // assets
 import favorite from '../../assets/icons/favorite.svg'
 import favorited from '../../assets/icons/favorited.svg'
-import flip from '../../assets/icons/flip.svg'
-import trash from '../../assets/icons/trash.svg'
+import close from "../../assets/icons/close.svg";
+import show from "../../assets/icons/show-more.svg";
 
 const Icon = ({ category }) => {
   const icons = {
     Favorite: favorite,
     Favorited: favorited,
-    Flip: flip,
-    Trash: trash,
-  }
+    Close: close,
+    Show: show,
+  };
 
   return (
     <img className="icon" src={icons[category]} alt={`A ${category} icon.`} />
